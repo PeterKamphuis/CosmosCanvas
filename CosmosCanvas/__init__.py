79578b272c37a65a25491f596277754f12e0b5a8
Peter Kamphuis <peterkamphuisastronomy@gmail.com>
2026-06-17 17:25:23 +0200

    Make sure vmap, spx and gal ar available as top level module

---
+++ CosmosCanvas/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Mark Richardson, Jayanne English, Nathan Deg"
+__version__ = "2.0-dev1"
+
+from .code import galfits as gal
+from .code import specindex as spx
+from .code import velmap as vmap
